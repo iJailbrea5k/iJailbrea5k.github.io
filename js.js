3ae2db10279b7ced351b0da2f43c9408ca1843c6
iJailbrea5k <sa_8001@icloud.com>
2017-07-11 23:07:13 +0900

    Xiaomi Story File Document

--- js.js
+++ js.js
@@ -2,15 +2,18 @@ function elppa(i) {
     if (i == 0) {
         document.getElementById("ROM").style.display = "none";
         document.getElementById("xstable1").style.display = "none";
+        document.getElementById("xstable2").style.display = "none";
     }
     if (i == 1) {
         document.getElementById("xstitle").style.display = "none";
         document.getElementById("xstable1").style.display = "none";
+        document.getElementById("xstable2").style.display = "none";
 
     }
     if (i == 2) {
         document.getElementById("xstitle").style.display = "none";
         document.getElementById("ROM").style.display = "none";
+        document.getElementById("xstable2").style.display = "none";
     }
     if (i == 3) {
         document.getElementById("xstitle").style.display = "none";
